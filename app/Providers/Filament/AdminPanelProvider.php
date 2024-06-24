8b65a279058f24fed3b914e32f58e5680c553e15
Charles <charles@notilac.fr>
2024-06-24 09:18:30 +0200

    ajout du reset de password

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,6 +35,7 @@ class AdminPanelProvider extends PanelProvider
                     ->enableSanctumTokens()
             )
             ->login()
+            ->passwordReset() 
             ->colors([
                 'primary' => Color::Amber,
             ])
